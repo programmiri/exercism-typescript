4bbab77e652fbf6fcba634344e764a760ea95af8
Mirjam Bäuerlein <mirjam.baeuerlein@cosee.biz>
2019-07-02 14:35:01 +0200

    Solve excercise TwoFer

--- two-fer/two-fer.ts
+++ two-fer/two-fer.ts
@@ -1,7 +1,7 @@
 class TwoFer {
-  static twoFer( /* Parameters go here */ ) {
-    // Your code here
-  }
+    static twoFer(name: string = "you") {
+        return `One for ${name}, one for me.`;
+    }
 }
 
-export default TwoFer
+export default TwoFer;
