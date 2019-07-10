38bd6ab17c3ebb31195d2378f4120aff2d380c2c
programmiri <programmiri@gmail.com>
2019-07-10 22:07:49 +0200

    Refactore exercise Leap

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,14 +1,12 @@
 function evenlyDivisible(year: number, div: number) {
-    return year % div === 0;
+	return year % div === 0;
 }
 
 function isLeapYear(year: number) {
-    if (
-        evenlyDivisible(year, 4) &&
-        !(evenlyDivisible(year, 100) && !evenlyDivisible(year, 400))
-    )
-        return true;
-    return false;
+	return (
+		evenlyDivisible(year, 4) &&
+		!(evenlyDivisible(year, 100) && !evenlyDivisible(year, 400))
+	);
 }
 
 export default isLeapYear;
